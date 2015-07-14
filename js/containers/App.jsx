a4e3d0a09df2b3aa4e4d7166ac07d1f3505ddcc0
Griffin Yourick <gryphon92@gmail.com>
2015-07-14 19:26:04 -0400

    Use composition when defining 'custom' createStore method

--- js/containers/App.jsx
+++ js/containers/App.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { provide } from 'react-redux';
 import Router, { Redirect, Route } from 'react-router';
 import { history } from 'react-router/lib/BrowserHistory';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import { TodoApp } from '../components';
 import * as reducers from '../reducers';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const composedCreateStore = compose(
+  applyMiddleware(thunk),
+  createStore
+);
 const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
+const store = composedCreateStore(reducer);
 
 @provide(store)
 export default class App extends Component {
