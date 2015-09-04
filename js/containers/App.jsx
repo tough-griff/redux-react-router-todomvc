5e33b70b7869ea281d8be8a554df4c478e55ed4c
Griffin Yourick <gryphon92@gmail.com>
2015-09-04 15:10:28 -0400

    Update package requirements
    Fixes #2

--- js/containers/App.jsx
+++ js/containers/App.jsx
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        {() => <TodoRouter />}
+        <TodoRouter />
       </Provider>
     );
   }
